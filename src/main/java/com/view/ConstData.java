106ebecef78c5604eefffc7716476d6e68829951
PRAMOD BEGUR NAGARAJ <begurnagaraj.p@northeastern.edu>
2022-12-01 22:50:16 -0500

    Data Manuplation

---
+++ src/main/java/com/view/ConstData.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.view;
+
+import com.entity.LoginDetails;
+
+/**
+ *
+ * @author ajitpatil
+ */
+public class ConstData {
+        public static final String PATIENT = "PATIENT";
+    
+   // public static String currentUserRole = null;
+    public static LoginDetails currentUser;
+    
+}
